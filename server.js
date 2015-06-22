76bce5e66ab400adcae1832693fc85186efa1863
swarupasv <vaishampayan4@gmail.com>
2015-06-22 16:27:32 +0530

    added simple structure enviornment

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ app.use(express.static('public'));
 //routes
 app.get('/',function(req,res)
 {
-	res.render('index');
+	res.render('index');csvfdddv
 });
+
 //req=request => http request object
 //res=response => http response object
 app.listen(3020);
 console.log('Express server is listening at 3020');
+
